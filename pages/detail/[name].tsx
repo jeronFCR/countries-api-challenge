1f365a7577f28c6b122af6a347264bb39cf6bb19
Jeron Fernandez Caballero <jeronfcr@gmail.com>
2021-12-12 20:54:05 +0100

    [page]: show null if no country selected

--- pages/detail/[name].tsx
+++ pages/detail/[name].tsx
@@ -35,67 +35,82 @@ const CountryDetailPage: NextPage = () => {
         </div>
       </Grid>
       <Grid $col={2} $colXS={1} $colMD={1} $gapY={false} $paddingY={false}>
-        <Flag
-          $click={false}
-          $detail={true}
-          src={c.flags.svg}
-          alt={c.name.common}
-        />
-        <CountryDetail>
-          <CountryName $detail>{c.name.common}</CountryName>
-          <Grid $col={2} $colXS={1} $colMD={1} $gapX={false} $gapY={false}  $paddingX={false} $paddingY={false}>
-            <div>
-              <Typography $detail>
-                <FieldTitle>Native Name: </FieldTitle>
-                {c.name.nativeName[Object.keys(c.name.nativeName)[0]].common}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Population: </FieldTitle>
-                {c.population.toLocaleString()}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Region: </FieldTitle>
-                {c.region}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Sub Region: </FieldTitle>
-                {c.subregion}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Capital: </FieldTitle>
-                {c.capital}
-              </Typography>
-            </div>
-            <div>
-              <Typography $detail>
-                <FieldTitle>Top Level Domain: </FieldTitle>
-                {c.tld.join(", ")}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Currencies: </FieldTitle>
-                {Object.keys(c.currencies)
-                  .map((key: string) => c.currencies[key].name)
-                  .join(", ")}
-              </Typography>
-              <Typography $detail>
-                <FieldTitle>Languages: </FieldTitle>
-                {Object.keys(c.languages)
-                  .map((key: string) => c.languages[key])
-                  .join(", ")}
-              </Typography>
-            </div>
-          </Grid>
-          {c.borders ? (
-            <Typography $detail>
-              <FieldTitle>Border Countries: </FieldTitle>
-              {c.borders.map((b: string) => (
-                <Chip key={uuidv4()} $click={false}>
-                  {b}
-                </Chip>
-              ))}
-            </Typography>
-          ) : null}
-        </CountryDetail>
+        {c ? (
+          <React.Fragment>
+            <Flag
+              $click={false}
+              $detail={true}
+              src={c.flags.svg}
+              alt={c.name.common}
+            />
+            <CountryDetail>
+              <CountryName $detail>{c.name.common}</CountryName>
+              <Grid
+                $col={2}
+                $colXS={1}
+                $colMD={1}
+                $gapX={false}
+                $gapY={false}
+                $paddingX={false}
+                $paddingY={false}
+              >
+                <div>
+                  <Typography $detail>
+                    <FieldTitle>Native Name: </FieldTitle>
+                    {
+                      c.name.nativeName[Object.keys(c.name.nativeName)[0]]
+                        .common
+                    }
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Population: </FieldTitle>
+                    {c.population.toLocaleString()}
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Region: </FieldTitle>
+                    {c.region}
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Sub Region: </FieldTitle>
+                    {c.subregion}
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Capital: </FieldTitle>
+                    {c.capital}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography $detail>
+                    <FieldTitle>Top Level Domain: </FieldTitle>
+                    {c.tld.join(", ")}
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Currencies: </FieldTitle>
+                    {Object.keys(c.currencies)
+                      .map((key: string) => c.currencies[key].name)
+                      .join(", ")}
+                  </Typography>
+                  <Typography $detail>
+                    <FieldTitle>Languages: </FieldTitle>
+                    {Object.keys(c.languages)
+                      .map((key: string) => c.languages[key])
+                      .join(", ")}
+                  </Typography>
+                </div>
+              </Grid>
+              {c.borders ? (
+                <Typography $detail>
+                  <FieldTitle>Border Countries: </FieldTitle>
+                  {c.borders.map((b: string) => (
+                    <Chip key={uuidv4()} $click={false}>
+                      {b}
+                    </Chip>
+                  ))}
+                </Typography>
+              ) : null}
+            </CountryDetail>
+          </React.Fragment>
+        ) : null}
       </Grid>
     </React.Fragment>
   );
