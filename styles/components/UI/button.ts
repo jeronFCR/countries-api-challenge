1c8259dd7892f757fbc0e6fcb1d75369fc34754a
Jeron Fernandez Caballero <jeronfcr@gmail.com>
2021-12-12 23:27:47 +0100

    [style]: Removed unnecessary styles in button

--- styles/components/UI/button.ts
+++ styles/components/UI/button.ts
@@ -9,10 +9,6 @@ export const Button = styled(ButtonComponent)<{ $shadow?: boolean }>`
   border: 0px;
   padding: 10px 30px;
   border-radius: 4px;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   ${({ $shadow }) =>
     $shadow === false
       ? null
@@ -20,7 +16,6 @@ export const Button = styled(ButtonComponent)<{ $shadow?: boolean }>`
     rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;`}
 
   & span {
-    display: inherit;
     margin-right: 8px;
     margin-left: -4px;
   }
