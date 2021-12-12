8bbddbbd0e5b08964f2cafecb2e2c3edc4e5cff6
Jeron Fernandez Caballero <jeronfcr@gmail.com>
2021-12-12 23:17:49 +0100

    [style]: margin between elements in country detail when md view or less

--- styles/components/country/countryDetail.ts
+++ styles/components/country/countryDetail.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components";
+import { Grid } from "../UI/grid";
 
 export const CountryDetail = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+
+  @media (max-width: 700px) {
+    margin-top: 30px;
+
+    & ${Grid} > * {
+      margin: 10px 0;
+    }
+  }
 `;
